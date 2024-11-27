9213c72a62ca172aec8f22f6131b20037a381bd0
Gaurav Nemani <gaurav1nemani@gmail.com>
2024-11-27 13:07:17 +0100

    Update GetBullsNotBears.py

--- GetBullsNotBears.py
+++ GetBullsNotBears.py
@@ -322,8 +322,7 @@ elif menu=="Monte Carlo Simulation":
         st.pyplot(plt)
         
         #Add VAR Value
-        ending_price = simulation_df.iloc[-1].squeeze()
-        ending_price = pd.to_numeric(ending_price, errors='coerce').dropna()
+        ending_price = simulation_df.iloc[-1]
         future_price_95ci = np.percentile(ending_price, 5)
         VaR = close_price[-1] - future_price_95ci
         st.write('Value at Risk (VaR) at 95% confidence interval is: ' + str(np.round(VaR, 2)) + ' USD')
